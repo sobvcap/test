c48bcab9e81096318ba09c14d0a5f9e1cf5721e5
sobvcap <129061073+sobvcap@users.noreply.github.com>
2023-03-27 15:29:56 +0100

    Add files via upload

---
+++ Test1.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[27]:
+
+
+import plotly.graph_objects as go
+import dash
+from dash import html
+import pandas as pd
+import plotly.offline as pyo
+from dash import dcc
+import numpy as np
+#from PIL import Image
+import plotly.express as px
+import gunicorn
+import dash_auth
+
+
+# In[28]:
+
+
+app = dash.Dash()
+#pil_image = Image.open("Velox Symbol.png")
+server = app.server
+#v_symbol = html.Img(src=pil_image, style={'height':'10%', 'width':'10%', 'padding-left':'42%', 'padding-right':'27%'})
+df = px.data.iris()  # iris is a pandas DataFrame
+fig = px.scatter(df, x="sepal_width", y="sepal_length")
+VALID_USERNAME_PASSWORD_PAIRS = {
+    'hello': 'world'
+}
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
+app.layout = html.Div([
+                      
+                      html.Div('Percentage Exposure',style= {
+                              'color': 'black',
+                          "bottom": "0px",
+                          'text-align': 'left',
+                           "font-size":"50px",
+                                }),
+                       
+                        html.Div('GROSS NET/5D AV NET',style= {
+                              'color': 'black',
+                             "font-size":"50px",
+                          "bottom": "10px",
+                          'text-align': 'right'
+                                }),
+                        #dcc.Slider(0, 20, 2.5,
+                         #  value=10,
+                          #id='my-slider'
+                           # )
+                       dcc.Graph(figure=fig)
+
+                      
+                      
+                      
+                      
+                      ])
+
+if __name__ == '__main__':
+    #serve(app,host='0.0.0.0',port=63349)
+    app.run_server()
+
+
+# In[26]:
+
+
+get_ipython().system('pip install dash_auth')
+
+
+# In[ ]:
+
+
+
+
